e78224171647fce200e8c17af212171547ac255c
alimukhtar53 <alimukhtar52@gmail.com>
2023-04-01 19:14:00 +0500

    Implement container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import SvgData from "./components/SvgData";
-import { Box, HStack, Text, Input, Grid } from "@chakra-ui/react";
+import { Box, Container, HStack, Input, VStack } from "@chakra-ui/react";
 import Swatches from "./components/Swatches";
 
 function App() {
@@ -10,19 +10,25 @@ function App() {
     setColor(e.target.value);
   };
   return (
-    <>
-      <SvgData color={color} width={512} height={512}></SvgData>
-      <Swatches />
-      <Input
-        width={"400px"}
-        height={"100px"}
-        border={"none"}
-        padding={0}
-        type="color"
-        value={color}
-        onChange={onColorChangeHandler}
-      ></Input>
-    </>
+    <VStack className="container">
+      <Container maxW="1200px">
+        <HStack>
+          <Box maxW={"512px"}>
+            <SvgData color={color} width={512} height={512}></SvgData>
+          </Box>
+          <Swatches />
+        </HStack>
+        <Input
+          width={"400px"}
+          height={"100px"}
+          border={"none"}
+          padding={0}
+          type="color"
+          value={color}
+          onChange={onColorChangeHandler}
+        ></Input>
+      </Container>
+    </VStack>
   );
 }
 
