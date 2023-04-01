d844d6d9689a1c79f8dfbca386f867966b3759eb
alimukhtar53 <alimukhtar52@gmail.com>
2023-04-01 18:06:24 +0500

    Rendered svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import SvgData from "./components/SvgData";
+import { Input } from "@chakra-ui/react";
+
 function App() {
-  return <div></div>;
+  const [color, setColor] = useState("000000");
+
+  const onColorChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(e.target.value);
+  };
+  return (
+    <>
+      <SvgData color={color} width={512} height={512}></SvgData>
+    </>
+  );
 }
 
 export default App;
