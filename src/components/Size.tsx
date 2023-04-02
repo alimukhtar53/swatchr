6f90b79c0f64cc97fe2a6c0dbc6b684392f49b74
alimukhtar53 <alimukhtar52@gmail.com>
2023-04-02 07:55:11 +0500

    Added more values in size component

--- src/components/Size.tsx
+++ src/components/Size.tsx
@@ -1,10 +1,11 @@
-import { Box, Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
+import { Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
 
 interface Props {
   onSizeSelect: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
 const Size = ({ onSizeSelect }: Props) => {
+  const size = "lg";
   return (
     <div>
       <Text textAlign={"left"} fontSize={"2xl"} pb={3}>
@@ -12,13 +13,19 @@ const Size = ({ onSizeSelect }: Props) => {
       </Text>
       <RadioGroup>
         <Stack direction="row" gap={2} onClick={onSizeSelect}>
-          <Radio value="32" size={"lg"}>
+          <Radio value="32" size={size}>
             32
           </Radio>
-          <Radio value="64" size={"lg"}>
+          <Radio value="64" size={size}>
             64
           </Radio>
-          <Radio value="512" size={"lg"}>
+          <Radio value="128" size={size}>
+            128
+          </Radio>
+          <Radio value="256" size={size}>
+            256
+          </Radio>
+          <Radio value="512" size={size}>
             512
           </Radio>
         </Stack>
